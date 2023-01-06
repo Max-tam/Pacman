4244b9641ab0c9726f16b5d4b42eed3db8f607e2
gavril <115543708+Gavril13@users.noreply.github.com>
2023-01-06 12:10:33 +0100

    Parametre

---
+++ prog8/En-têtes/params.h
@@ -0,0 +1,10 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+#include "En-têtes/type.h"
+
+void InitParams (CMyParam & Param); //problème a voir
+
+void LoadParams (CMyParam & Param); //probleme a voir
+
+#endif // PARAMS_H
